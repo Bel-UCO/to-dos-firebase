1e282ebce4064fd090b80054cbc2242a9b116ea8
Bel-UCO <bpoetridhanti@student.ciputra.ac.id>
2026-03-17 00:38:26 +0700

    env installation

--- project/firebase_config.php
+++ project/firebase_config.php
@@ -1,7 +1,25 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$serviceAccount = [
+    "type" => $_ENV['FIREBASE_TYPE'],
+    "project_id" => $_ENV['FIREBASE_PROJECT_ID'],
+    "private_key_id" => $_ENV['FIREBASE_PRIVATE_KEY_ID'],
+    "private_key" => str_replace("\\n", "\n", $_ENV['FIREBASE_PRIVATE_KEY']),
+    "client_email" => $_ENV['FIREBASE_CLIENT_EMAIL'],
+    "client_id" => $_ENV['FIREBASE_CLIENT_ID'],
+    "auth_uri" => $_ENV['FIREBASE_AUTH_URI'],
+    "token_uri" => $_ENV['FIREBASE_TOKEN_URI'],
+    "auth_provider_x509_cert_url" => $_ENV['FIREBASE_AUTH_PROVIDER_CERT_URL'],
+    "client_x509_cert_url" => $_ENV['FIREBASE_CLIENT_CERT_URL'],
+    "universe_domain" => $_ENV['FIREBASE_UNIVERSE_DOMAIN']
+];
 
 // // Ambil credentials dari environment variable
 // $firebaseCredentials = getenv('FIREBASE_CREDENTIALS');
@@ -19,8 +37,8 @@ use Kreait\Firebase\Factory;
 
 // Konfigurasi Firebase (Ganti dengan file JSON yang diunduh dari Firebase)
 $factory = (new Factory)
-    ->withServiceAccount(__DIR__ . '/../firebase_credentials.json') // Ganti dengan file JSON Firebase Anda
-    ->withDatabaseUri('https://afl-2-cc-default-rtdb.asia-southeast1.firebasedatabase.app/'); // Ganti dengan URL database Anda
+    ->withServiceAccount($serviceAccount) // Ganti dengan file JSON Firebase Anda
+    ->withDatabaseUri($_ENV['FIREBASE_DATABASE_URI']); // Ganti dengan URL database Anda
 
 $database = $factory->createDatabase();
 $auth = $factory->createAuth();
